776fe11595e42bd46787edcb8427d68bcc3c048f
Roi Driscoll <roidriscoll@MASTAFF-Roi-Driscoll-PX26YJ6409.local>
2022-12-01 11:45:53 +0100

    Adds file with todo list

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+# Given a series of numbers from 0..n
+# For each number
+# Check that the number is
+#   divisible by three
+#     if so return "Fizz"
+#   divisible by five
+#     if so return "Buzz"
+#   divisible by both
+#     if so return "FizzBuzz"
+#   otherwise
+#       return the original number (as a string)
+
